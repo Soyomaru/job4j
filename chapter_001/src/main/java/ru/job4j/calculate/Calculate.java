574a4fa109e7da721e32347fe07eee80abdd7bf0
Soyomaru <soyomaru@ya.ru>
2018-09-10 23:08:07 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j.calculate;
 
+/**
+* First step in use javadoc for job4j 
+* commenting class <strong>Calculate</strong>
+*/
+
+
 public class Calculate{
+
+/** Method main have arguments @param args
+*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
