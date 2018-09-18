16bbec9cbf064c686259ca47aaeeca6e9ab7b2aa
Soyomaru <soyomaru@ya.ru>
2018-09-18 23:11:04 +0300

    3.4. Расстояние между точками в системе координат [#188] Окончательный вариант

--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -5,12 +5,12 @@ import static org.hamcrest.number.IsCloseTo.closeTo;
 import static org.junit.Assert.assertThat;
 
 
-public class PointWithTest {
+public class PointTest {
 
     @Test
     public void tochkiDistant() {
-        PointWith a = new PointWith(0, 1);
-        PointWith b = new PointWith(2, 5);
+        Point a = new Point(0, 1);
+        Point b = new Point(2, 5);
         double result = a.distanceTo(b);
         assertThat(result, closeTo(4.47, 0.1));
     }
