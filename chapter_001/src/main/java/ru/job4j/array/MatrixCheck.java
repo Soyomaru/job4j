09352b4ea265baaac216953475a052d9c39ab00b
Soyomaru <soyomaru@ya.ru>
2018-11-16 16:11:27 +0300

    6.7. Квадратный массив заполнен true или false по диагоналям. Убрал дублирующий цикл и перенес проверку из второго цикла в первый

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -4,20 +4,11 @@ public class MatrixCheck {
     public boolean mono(boolean[][] data) {
         boolean result = true;
 
-//прямая диагональ
         for (int index = 1; (index < data.length) && (index < data[0].length); index++) {
             if (data[index][index] != data[index - 1][index - 1]) {
                 result = false;
                 break;
             }
-        }
-
-        if (!result) {
-            return result;
-        }
-
-//обратная диагональ
-        for (int index = 1; (index < data.length) && (index < data[0].length); index++) {
             if (data[index][data[index].length - index - 1] != data[index - 1][data[index - 1].length - index]) {
                 result = false;
                 break;
